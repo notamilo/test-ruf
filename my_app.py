970a2451d38b5e140bbdbe845fe3ae1373aaa4b0
notamilo <127539604+notamilo@users.noreply.github.com>
2023-03-23 21:05:59 +0200

    Add files via upload
    Перше вікно повністю і всі змінні

---
+++ my_app.py
@@ -0,0 +1,44 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
+from instr import *
+from second_win import *
+
+
+class MainWin(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+        self.set_appear()
+        self.show()
+    
+    
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+
+
+    def initUI(self):
+        self.hello_text = QLabel(txt_hello)
+        self.instruction = QLabel(txt_instruction)
+        self.button = QPushButton(txt_next)
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.hello_text, alignment = Qt.AlignCenter)
+        self.layout.addWidget(self.instruction, alignment = Qt.AlignCenter)
+        self.layout.addWidget(self.button)
+        self.connects()
+
+        return self.layout
+    
+    def next_click(self):
+        self.tw = TestWin()
+        self.hide()
+
+    
+    def connects(self):
+        self.button.clicked.connect(self.next_click)
+
+
+app = QApplication([])
+mw = MainWin()
+mw.setLayout(mw.initUI())
+app.exec_()
